94a95a250e80a3f13557176c116189a5373de50e
mohamed <mohamed.aden@live.ca>
2020-05-26 16:05:52 -0400

    basic HTML completed, along with some CSS

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// /*----- app state -----*/
+// let board, winner, turn;
+
+// /*----- cached elements -----*/
+// const msgEl = document.getElementById(ENTER MESSAGE ELEMENT)
+// const markersEl(the trigger) = document.getElementById
+
+// const cellEls
+
+// /* events */
+
+// on click erc
+// /*----- function ------*/
+// init() - getting the game going
+
+// function render() {
+//     //if letter used then grey out button - .style.color = .includes ? grey : blue
+
+// }
+
+
+// function init() {
+
+//     resets game
+
+
+//     array.forEach() => { //looping through array
+//     });
+// winner = null
+
+// render()
+// }
+
+// render
+//     -
+//     hide my letters as I click them -
+
+$(document).ready(function() {
+
+    const listClick = $("ul")
+
+    listClick.click(function(x) {
+        listClick.addClass("active");
+    })
+})
