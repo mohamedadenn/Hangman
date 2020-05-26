f64fa0c5a9b5bd3a6076ee09b6ab462d5012a62e
mohamed <mohamed.aden@live.ca>
2020-05-26 18:51:32 -0400

    added hangman canvas draw and clear functions

--- js/main.js
+++ js/main.js
@@ -3,12 +3,26 @@ $(document).ready(function() {
     let win, lose; //scorekeeping varaibles
     let category = ["fruits"]
     let words = ["apples", "bananas", "kiwi"];
+    let hint = [""];
+    const draws = [
+        'gallows',
+        'head',
+        'body',
+        'rightHarm',
+        'leftHarm',
+        'rightLeg',
+        'leftLeg',
+        'rightFoot',
+        'leftFoot',
+    ]
+    var step = 0;
 
     /*------------ cached elements ---------------*/
 
     const status = $("#status");
     const selectedCategory = $("#selectedcategory");
-    const canvas = $("canvas");
+    const canvas = document.getElementById('hangman');
+    const context = canvas.getContext("2d");
     const makeUnderline = $("#wordwrap");
     const alphabet = $("li"); //.eq selects the specific one
     const resetButton = $("#reset");
@@ -29,23 +43,104 @@ $(document).ready(function() {
     })
 
     resetButton.click(function() {
-        console.log("reset")
+        Draw(draws[step++])
+        if (undefined === draws[step]);
     })
 
     hintButton.click(function() {
-        console.log("hint")
+        clearCanvas()
+        step = 0
     })
 
     soundButton.click(function() {
         console.log("sound")
     })
 
-    // on click erc
-    // /*----- function ------*/
-    // init() - getting the game going
+    /*----- HANGMAN DRAWING -------*/
+    Draw = (part) => {
+        switch (part) {
+            case 'gallows':
+                context.strokeStyle = '#444';
+                context.lineWidth = 10;
+                context.beginPath();
+                context.moveTo(175, 195);
+                context.lineTo(5, 195);
+                context.moveTo(40, 195);
+                context.lineTo(25, 5);
+                context.lineTo(100, 5);
+                context.lineTo(100, 25);
+                context.stroke();
+                break;
+
+            case 'head':
+                context.lineWidth = 5;
+                context.beginPath();
+                context.arc(100, 50, 25, 0, Math.PI * 2, true);
+                context.closePath();
+                context.stroke();
+                break;
+
+            case 'body':
+                context.beginPath();
+                context.moveTo(100, 75);
+                context.lineTo(100, 140);
+                context.stroke();
+                break;
+
+            case 'rightHarm':
+                context.beginPath();
+                context.moveTo(100, 85);
+                context.lineTo(60, 100);
+                context.stroke();
+                break;
+
+            case 'leftHarm':
+                context.beginPath();
+                context.moveTo(100, 85);
+                context.lineTo(140, 100);
+                context.stroke();
+                break;
+
+            case 'rightLeg':
+                context.beginPath();
+                context.moveTo(100, 140);
+                context.lineTo(80, 190);
+                context.stroke();
+                break;
+
+            case 'rightFoot':
+                context.beginPath();
+                context.moveTo(82, 190);
+                context.lineTo(70, 185);
+                context.stroke();
+                break;
+
+            case 'leftLeg':
+                context.beginPath();
+                context.moveTo(100, 140);
+                context.lineTo(125, 190);
+                context.stroke();
+                break;
+
+            case 'leftFoot':
+                context.beginPath();
+                context.moveTo(122, 190);
+                context.lineTo(135, 185);
+                context.stroke();
+                break;
+        }
+    }
+
+    /*----- function ------*/
+    clearCanvas = () => {
+        context.clearRect(0, 0, canvas.width, canvas.height)
+    }
+
+    // init() //initalizes all state variables
 
     // function render() {
-    //     //if letter used then grey out button - .style.color = .includes ? grey : blue
+    //     //     //if letter used then grey out button - .style.color = .includes ? grey : blue
+    //     if
 
     // }
 
